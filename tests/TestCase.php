bee117c5fcc1a2ba9d99990adf78a70c7de00041
atendwa <atendwa@gmail.com>
2025-06-01 20:19:05 +0300

    fix(tests): updates service provider import

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Atendwa\Filakit\Tests;
 
+use Atendwa\Filakit\Providers\FilakitServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Atendwa\Filakit\FilakitServiceProvider;
 
 class TestCase extends Orchestra
 {
