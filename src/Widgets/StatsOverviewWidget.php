ab79afd672a16ba970c4acba0a2b25cf3f0a7266
atendwa <125092307+atendwa@users.noreply.github.com>
2025-06-22 15:59:34 +0000

    Fix styling

--- src/Widgets/StatsOverviewWidget.php
+++ src/Widgets/StatsOverviewWidget.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace Atendwa\Filakit\Widgets;
 
-use Atendwa\Filakit\Concerns\UsesLazyWidget;
-
 class StatsOverviewWidget extends \Filament\Widgets\StatsOverviewWidget
 {
+    public int $columns = 2;
+
     protected static ?string $pollingInterval = null;
 
     protected static bool $isLazy = false;
-
-    public int $columns = 2;
 
     protected function getColumns(): int
     {
