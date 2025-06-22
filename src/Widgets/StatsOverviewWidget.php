fcc9c0c38a0a359464a1a13a6f464fd0cf264ca5
atendwa <atendwa@gmail.com>
2025-06-22 19:01:11 +0300

    chore(concerns): makes columns a public property

--- src/Widgets/StatsOverviewWidget.php
+++ src/Widgets/StatsOverviewWidget.php
@@ -8,7 +8,9 @@ use Atendwa\Filakit\Concerns\UsesLazyWidget;
 
 class StatsOverviewWidget extends \Filament\Widgets\StatsOverviewWidget
 {
-    use UsesLazyWidget;
+    protected static ?string $pollingInterval = null;
+
+    protected static bool $isLazy = false;
 
     public int $columns = 2;
 
