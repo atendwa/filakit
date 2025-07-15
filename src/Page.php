6367dab15466246b1601ee43ffaa44c142e0775b
atendwa <atendwa@gmail.com>
2025-07-15 21:00:08 +0300

    chore(concerns): minor change

--- src/Page.php
+++ src/Page.php
@@ -2,9 +2,29 @@
 
 namespace Atendwa\Filakit;
 
+use Atendwa\Filakit\Concerns\HasRefreshAction;
 use BezhanSalleh\FilamentShield\Traits\HasPageShield;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
 
-class Page
+class Page extends \Filament\Pages\Page
 {
     use HasPageShield;
+    use HasRefreshAction;
+
+    /**
+     * @return Action[]|ActionGroup[]
+     */
+    public function actions(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return Action[]|ActionGroup[]
+     */
+    protected function getHeaderActions(): array
+    {
+        return collect($this->actions())->push($this->getRefreshAction())->all();
+    }
 }
